35c117e59aa106988b1f55cbf301aa3a83f799a3
Wing <wzchan@myseneca.ca>
2025-03-04 17:45:10 -0500

    Add Item link

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+
+WEB322 – Assignment 02
+I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
+
+Name: Wing Zee Chan 
+Student ID: 180683237 
+Date: Feb-5-2025
+Render Web App URL: https://web322-app-rli5.onrender.com
+GitHub Repository URL: https://github.com/nevami/web322-app
+
+********************************************************************************/ 
+
 // Import the Express module
 const express = require("express");
 
